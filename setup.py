f8a5495681ddfac2ebd9591d57a3cdfc5b0117c1
oldbay <old_bay@mail.ru>
2019-10-11 18:25:29 +0300

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wfs_geocoder', 
-    version='0.2', 
+    version='0.3', 
     packages=['wfs_geocoder'], 
     install_requires=[
         'OWSLib',
